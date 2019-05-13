7d2d604a8120b336422cab85344ecc3d28ea5349
Ирина Барчук <iabarchuk@edu.hse.ru>
2019-05-13 14:27:16 +0300

    Scaling completed

--- DeBoorsSplines/DeBoorsSplines/ScaleClass.cs
+++ DeBoorsSplines/DeBoorsSplines/ScaleClass.cs
@@ -44,32 +44,102 @@ namespace DeBoorsSplines
             }
         }
 
+        private void ChangeSplineX(SplineCollection splineCollection)
+        {
+            int indexOfTheClosest = 0;
+
+            // Модуль - расстояние от начала координат к х-координате точки
+            double module = splineCollection.PointsList[indexOfTheClosest].PointX;
+            
+            for(int i = 1; i < splineCollection.PointsList.Count(); i++)
+            {
+                if (splineCollection.PointsList[i].PointX < module)
+                {
+                    module = splineCollection.PointsList[i].PointX;
+
+                    indexOfTheClosest = i;
+                }
+            }
+
+            double idealX = 4,
+                changeOfX = splineCollection.PointsList[indexOfTheClosest].PointX - idealX;
+
+            for (int i = 0; i < splineCollection.PointsList.Count(); i++)
+            {
+                if(i != indexOfTheClosest)
+                {
+                    splineCollection.PointsList[i].PointX -= changeOfX;
+                }
+                else
+                {
+                    splineCollection.PointsList[i].PointX = idealX;
+                }
+            }
+        }
+
+        private void ChangeSplineY(SplineCollection splineCollection)
+        {
+            int indexOfTheClosest = 0;
+
+            // Модуль - расстояние от начала координат к х-координате точки
+            double module = splineCollection.PointsList[indexOfTheClosest].PointY;
+
+            for (int i = 1; i < splineCollection.PointsList.Count(); i++)
+            {
+                if (splineCollection.PointsList[i].PointY < module)
+                {
+                    module = splineCollection.PointsList[i].PointY;
+
+                    indexOfTheClosest = i;
+                }
+            }
+
+            double idealY = 4,
+                changeOfY = splineCollection.PointsList[indexOfTheClosest].PointY - idealY;
+
+            for (int i = 0; i < splineCollection.PointsList.Count(); i++)
+            {
+                if (i != indexOfTheClosest)
+                {
+                    splineCollection.PointsList[i].PointY -= changeOfY;
+                }
+                else
+                {
+                    splineCollection.PointsList[i].PointY = idealY;
+                }
+            }
+        }
+
         public void ScaleSpline(MainWindow mainWindow, SplineCollection splineCollection)
         {
+            ChangeSplineX(splineCollection);
+            ChangeSplineY(splineCollection);
+
             double oldWidth = mainWindow.DeBoorsSplinesAppWindow.ActualWidth / 100 * 70,
-                oldHeight = mainWindow.DrawingCanvas.Height - 10 - 25 - 10-mainWindow.MenuStrip.ActualHeight,
+                oldHeight = mainWindow.DrawingCanvas.Height - 65 -mainWindow.MenuStrip.ActualHeight,
                 newWidth = 0,
                 newHeight = 0;
 
             if (splineCollection.PointsList[ScalingPointIndex].PointX >= oldWidth)
             {
-                newWidth = splineCollection.PointsList[ScalingPointIndex].PointX+30;
+                newWidth = splineCollection.PointsList[ScalingPointIndex].PointX+8;
 
-                newHeight = oldHeight * newWidth / oldWidth;
+                //newHeight = oldHeight * newWidth / oldWidth;
             }
-            else
+            if(splineCollection.PointsList[ScalingPointIndex].PointY >= oldHeight)
             {
-                newHeight = splineCollection.PointsList[ScalingPointIndex].PointY+30;
+                newHeight = splineCollection.PointsList[ScalingPointIndex].PointY+8;
 
-                newWidth = oldWidth * newHeight / oldHeight;
+                //newWidth = oldWidth * newHeight / oldHeight;
             }
 
-            double coeff = oldWidth / newWidth;
+            double coeffX = oldWidth / newWidth,
+                coeffY = oldHeight/newHeight;
 
             for (int i = 0; i < splineCollection.PointsList.Count(); i++)
             {
-                splineCollection.PointsList[i].PointX = (int)(coeff* splineCollection.PointsList[i].PointX);
-                splineCollection.PointsList[i].PointY = (int)(coeff* splineCollection.PointsList[i].PointY);
+                splineCollection.PointsList[i].PointX = (int)(coeffX* splineCollection.PointsList[i].PointX);
+                splineCollection.PointsList[i].PointY = (int)(coeffY* splineCollection.PointsList[i].PointY);
             }
         }
     }
